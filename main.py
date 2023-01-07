2f9b6ae36e2db0004dbe313633772891a6295b24
DongGiLee <donggi1012@gmail.com>
2023-01-08 00:29:43 +0900

    Declare Request Example Data

--- main.py
+++ main.py
@@ -1,49 +1,107 @@
 from fastapi import FastAPI, Query, Path, Body
-from typing import Union, List, Set, Dict
-from pydantic import BaseModel, Field, HttpUrl
+from typing import Union
+from pydantic import BaseModel, Field
 
 app = FastAPI()
-
-class Image(BaseModel):
-    url: HttpUrl
-    name: str
 
 class Item(BaseModel):
     name: str
     description: Union[str, None] = None
     price: float
     tax: Union[float, None] = None
-    tags: List[str] = []    # 3.9 이상 list[str]
-    tags2: Set[str] = set()
-    image: Union[Image, None] = None
-    images: Union[List[Image], None] = None
 
-class Offer(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    item: List[Item]
-
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 35.2,
+                "tax": 3.2,
+            }
+        }
+class Item2(BaseModel):
+    name: str = Field(example="Foo")
+    description: Union[str, None] = Field(default=None, example= "A very nice Item")
+    price: float = Field(example=30.5)
+    tax: Union[float, None] = Field(default=None, example=3.1)
 @app.get(
     path="/",
-    description="Step 2, Request Body Nested Models"
+    description="Step 2, Declare Request Example Data"
 )
 async def main():
     return {"Hello":"World"}
 
-@app.post(
-    path='/offers/'
+@app.put(
+    path="/items/{item_id}"
 )
-async def create_offer(offer: Offer):
-    return offer
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results
 
-@app.post(
-    path='/images/multiple/'
+@app.put(
+    path="/items2/{item_id}"
 )
-async def create_multiple_images(images: List[Image]):
-    return images
+async def update_item(item_id: int, item: Item2):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+@app.put(
+    path='/items3/{item_id}',
+    description="body example"
+)
+async def body_example(
+        item_id: int,
+        item: Item = Body(
+            example={
+                "name": "Boo",
+                "description":"Body Example",
+                "price": 24.4,
+                "tax": 2.1
+            }
+        )
+):
+    results = {"item_id":item_id, "item": item}
+
+    return results
+@app.put(
+    path='/items4/{item_id}',
+    description="body examples"
+)
+async def body_examples(
+        item_id: int,
+        item: Item = Body(
+            examples={
+                "normal": {
+                    "summary": "A normal example",
+                    "description": "A **normal** item works correctly.",
+                    "value": {
+                        "name": "Foo",
+                        "description": "A very nice Item",
+                        "price": 35.4,
+                        "tax": 3.2,
+                    },
+                },
+                "converted": {
+                    "summary": "An example with converted data",
+                    "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                    "value": {
+                        "name": "Bar",
+                        "price": "35.4",
+                    },
+                },
+                "invalid": {
+                    "summary": "Invalid data is rejected with an error",
+                    "value": {
+                        "name": "Baz",
+                        "price": "thirty five point four",
+                    },
+                },
+            }
+        )
+):
+    results = {"item_id":item_id, "item": item}
+
+    return results
 
 
-@app.post('/index-weights/')
-async def create_index_weights(weights: Dict[int, float]):
-    return weights
+
