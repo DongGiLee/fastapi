9313abab5b5a93a55667fbe68c7b5585b382bd21
DongGiLee <donggi1012@gmail.com>
2023-01-07 23:17:11 +0900

    Body - Nested Models

--- main.py
+++ main.py
@@ -1,43 +1,49 @@
 from fastapi import FastAPI, Query, Path, Body
-from typing import Union
-from pydantic import BaseModel, Field
+from typing import Union, List, Set, Dict
+from pydantic import BaseModel, Field, HttpUrl
 
 app = FastAPI()
 
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
+
 class Item(BaseModel):
     name: str
-    description: Union[str, None] = Field(
-        default=None,
-        title="The description of the item",
-        max_length=300
-    )
-    price: float = Field(
-        gt=0,
-        description="The price must be greater than zero"
-    )
+    description: Union[str, None] = None
+    price: float
     tax: Union[float, None] = None
+    tags: List[str] = []    # 3.9 이상 list[str]
+    tags2: Set[str] = set()
+    image: Union[Image, None] = None
+    images: Union[List[Image], None] = None
 
-class User(BaseModel):
-    username: str
-    full_name: Union[str, None] = None
+class Offer(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    item: List[Item]
 
 @app.get(
     path="/",
-    description="Step 2, Request Body Fields"
+    description="Step 2, Request Body Nested Models"
 )
 async def main():
     return {"Hello":"World"}
 
-@app.put(
-    path="/items/{item_id}",
-    description="Body Field"
+@app.post(
+    path='/offers/'
 )
-async def update_item(
-        item_id: int,
-        item: Item = Body(embed=True)
-):
-    results = { "item_id": item_id, "item": item}
+async def create_offer(offer: Offer):
+    return offer
 
-    return results
+@app.post(
+    path='/images/multiple/'
+)
+async def create_multiple_images(images: List[Image]):
+    return images
 
 
+@app.post('/index-weights/')
+async def create_index_weights(weights: Dict[int, float]):
+    return weights
