e8d2235673c9c9afa6a9220110af0b5aa80d42b5
DongGiLee <donggi1012@gmail.com>
2023-01-08 18:49:23 +0900

    Header Parameters

--- step3.py
+++ step3.py
@@ -1,12 +1,32 @@
-from typing import Union
-from fastapi import FastAPI, Body, Cookie
+from typing import Union, List
+from fastapi import FastAPI, Header
 
 app = FastAPI()
 
-
-@app.put(
+@app.get(
     path="/items/",
-    description="Cookie로 불러오지않은면 쿼리 매개변수로 해석됨"
+    description="Header,"
 )
-async def read_items(ads_id: Union[str, None] = Cookie(default=None)):
-    return {"ads_id": ads_id}
+async def read_items(user_agent: Union[str, None] = Header(default=None)):
+    return {"User-Agent": user_agent}
+
+@app.get(
+    path="/under_score/",
+    description="Header - convert_underscores=False: 언더스코어 하이픈 자동변환 비활성화"
+)
+async def underscore(
+        strange_header: Union[str, None] = Header(default=None, convert_underscores=False)
+):
+    return {"strange_header":strange_header}
+
+
+@app.get(
+    path="/duplicateheader/",
+    description="Header - 중복 헤더 "
+)
+async def duplicate_header(
+        x_token: Union[List[str], None] = Header(default=None)
+):
+    return {"X-Token values":x_token}
+
+
